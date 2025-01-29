5d68ba9b678e1d1afd0f9b7f75a5ea9ce4268f53
arrKhange1 <therock65frostik@gmail.com>
2025-01-29 18:30:36 +0500

    feat: add info card presentation and expanding row

--- src/cargo-report/component/report-table/report-table.component.ts
+++ src/cargo-report/component/report-table/report-table.component.ts
@@ -1,23 +1,39 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ReportUrlParamsBuilderService } from '../../service/report-url-params-builder.service';
 import { MatSortModule, Sort } from '@angular/material/sort';
 import { ReportSortService } from '../../service/report-sort.service';
-import { ReportSortUrlParams } from '../../model/report.model';
+import { ReportData, ReportSortUrlParams } from '../../model/report.model';
+import { NgTemplateOutlet } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatRipple } from '@angular/material/core';
 
 @Component({
   selector: 'app-report-table',
-  imports: [MatTableModule, MatSortModule],
+  imports: [MatTableModule, MatSortModule, NgTemplateOutlet, MatCardModule, MatListModule, MatRipple],
   templateUrl: './report-table.component.html',
   styleUrl: './report-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReportTableComponent {
+  private cdr = inject(ChangeDetectorRef);
   private reportSortService = inject(ReportSortService);
   private reportParamsBuilderService = inject(ReportUrlParamsBuilderService);
-  public reportData = toSignal(this.reportParamsBuilderService.getParameterizedFwbData(), { initialValue: [] });
-  public columnsToDisplay = ['prefix', 'serial', 'origin', 'destination', 'act_weight', 'unit'];
+  protected reportData = toSignal(this.reportParamsBuilderService.getParameterizedFwbData(), { initialValue: [] });
+  protected columnsToDisplay = ['prefix', 'serial', 'origin', 'destination', 'act_weight', 'unit'];
+  protected expandedRow: ReportData | null = null;
+
+  protected expandRowContent(row: ReportData) {
+    if (row !== this.expandedRow) {
+      console.log('row !== expandedRow');
+      this.expandedRow = row;
+    } else {
+      this.expandedRow = null;
+    }
+    this.cdr.markForCheck();
+  }
 
   protected sortChange({ active, direction }: Sort): void {
     const changedSortOrder = direction === '' ? null : direction;
