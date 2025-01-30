65d2d4a4921321034a63573c08ace09f82224eb7
arrKhange1 <therock65frostik@gmail.com>
2025-01-30 22:38:34 +0500

    refact: set header from config

--- src/cargo-report/component/saved-reports/saved-reports.component.ts
+++ src/cargo-report/component/saved-reports/saved-reports.component.ts
@@ -3,25 +3,12 @@ import { CdkDrag, CdkDragDrop, CdkDropList, copyArrayItem } from '@angular/cdk/d
 import { ReportData } from '../../model/report.model';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
-import { FwbDetailCardComponent } from '../detail-cards/fwb-detail-card/fwb-detail-card.component';
-import { AgentDetailCardComponent } from '../detail-cards/agent-detail-card/agent-detail-card.component';
-import { ConsigneeDetailCardComponent } from '../detail-cards/consignee-detail-card/consignee-detail-card.component';
-import { ShipperDetailCardComponent } from '../detail-cards/shipper-detail-card/shipper-detail-card.component';
 import { DetailCardsContainerComponent } from '../../container/detail-cards-container/detail-cards-container.component';
+import { REPORT_FIELDS_CONFIG } from '../../config/report-fields.config';
 
 @Component({
   selector: 'app-saved-reports',
-  imports: [
-    CdkDrag,
-    CdkDropList,
-    MatExpansionModule,
-    CdkAccordionModule,
-    FwbDetailCardComponent,
-    AgentDetailCardComponent,
-    ConsigneeDetailCardComponent,
-    ShipperDetailCardComponent,
-    DetailCardsContainerComponent,
-  ],
+  imports: [CdkDrag, CdkDropList, MatExpansionModule, CdkAccordionModule, DetailCardsContainerComponent],
   templateUrl: './saved-reports.component.html',
   styleUrl: './saved-reports.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -31,6 +18,7 @@ export class SavedReportsComponent {
   protected savedReportsEnteredPredicate: (dragEvent: CdkDrag<ReportData>) => boolean = (dragEvent) => {
     return !this.savedReports.includes(dragEvent.data);
   };
+  protected fwbDetailsConfig = REPORT_FIELDS_CONFIG.fwbDetails;
 
   protected drop(event: CdkDragDrop<ReportData[]>) {
     if (event.previousContainer !== event.container) {
