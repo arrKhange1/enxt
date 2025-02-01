3de43b3337d2c810e959bd94686a522e78e759b3
arrKhange1 <therock65frostik@gmail.com>
2025-02-01 16:29:28 +0500

    refact: add comments to api service and abstract response service

--- src/shared/api/report.api.service.ts
+++ src/shared/api/report.api.service.ts
@@ -11,6 +11,10 @@ export class ReportApiService extends AbstractResponseService {
   private baseUrl = 'https://enxtlinux.enxt.solutions:8013/api/Messages/FWBReports';
   private http = inject(HttpClient);
 
+  /**
+   * Метод для запроса на сервер за данными об отчете
+   * @param params - Готовые URL параметры
+   */
   public getReport(params: HttpParams): Observable<ServerResponse<FwbReport>> {
     return this.http.get<FwbReport>(`${this.baseUrl}`, { params }).pipe(
       map((res: FwbReport) => this.buildReadyValue(res)),
--- src/shared/helper/response.ts
+++ src/shared/helper/response.ts
@@ -6,11 +6,22 @@ export interface ServerResponse<T> {
   data?: T;
 }
 
+/**
+ * Класс, который наследуется API сервисами
+ * для построения ответа с сервера вместе с loading, error и data
+ * В конце запроса в ответ вставляются данные и поле isLoading = false,
+ * Если произошла ошибка
+ */
 export abstract class AbstractResponseService {
   getLoadingValue<T>(): ServerResponse<T> {
     return { isLoading: true };
   }
 
+  /**
+   * Метод, который вызывается после успешного выполнения запроса
+   * В конце запроса в ответ вставляются данные и поле isLoading = false
+   * @param data - Данные, который пришли с сервера
+   */
   public buildReadyValue<T>(data: T): ServerResponse<T> {
     return {
       isLoading: false,
@@ -18,10 +29,18 @@ export abstract class AbstractResponseService {
     };
   }
 
+  /**
+   * Operator Function, который вызывается в начале потока с запросом
+   * В начале запроса поле ответа isLoading сеттится в true
+   */
   public startWithLoading<T>(): OperatorFunction<ServerResponse<T>, ServerResponse<T>> {
     return startWith(this.getLoadingValue<T>());
   }
 
+  /**
+   * Operator Function, который вызывается, если запрос выполнился с ошибкой
+   * @returns Поток с ошибкой и isLoading = false
+   */
   public catchLoadingError<T>(): OperatorFunction<ServerResponse<T>, ServerResponse<T>> {
     return catchError((error) => {
       console.error('Error occurred:', error);
