015c6f93c015b89f332696c779b504e05f6af82c
arrKhange1 <therock65frostik@gmail.com>
2025-02-01 16:10:02 +0500

    refact: add comments to parameterized report service

--- src/cargo-report/service/parameterized-report.service.ts
+++ src/cargo-report/service/parameterized-report.service.ts
@@ -10,6 +10,13 @@ import { buildUrlParams } from '../../shared/helper/build-url-params';
 export class ParameterizedReportService {
   private urlParamsDataService = inject(ReportUrlParamsDataService);
   private reportApiService = inject(ReportApiService);
+
+  /**
+   * Поле, в котором подписываемся на URL параметры в памяти,
+   * делаем запрос на сервер за данными с этими параметрами,
+   * делимся параметризированными данными со всеми подписчиками
+   * @private
+   */
   private parameterizedReport$ = this.urlParamsDataService.reportUrlParams$.pipe(
     debounceTimeAfter(1, 250),
     map((reportUrlParams) => buildUrlParams({ ...reportUrlParams })),
@@ -17,6 +24,9 @@ export class ParameterizedReportService {
     shareReplay({ bufferSize: 1, refCount: true }),
   );
 
+  /**
+   * Возвращает поток с данными для таблицы
+   */
   public getParameterizedFwbData(): Observable<FwbData[]> {
     return this.parameterizedReport$.pipe(
       map((response) => response.data?.fwb_data),
@@ -24,6 +34,9 @@ export class ParameterizedReportService {
     );
   }
 
+  /**
+   * Возвращает поток с количеством строк данных
+   */
   public getParameterizedTotalRecords(): Observable<FwbReport['totalRecords']> {
     return this.parameterizedReport$.pipe(
       map((response) => response.data?.totalRecords),
@@ -31,6 +44,10 @@ export class ParameterizedReportService {
     );
   }
 
+  /**
+   * Возвращает поток со статусом загрузки данных с сервера
+   * Если в потоке true - данные еще идут с сервера, false - данные пришли
+   */
   public getReportLoadingState(): Observable<boolean> {
     return this.parameterizedReport$.pipe(map((response) => response.isLoading));
   }
