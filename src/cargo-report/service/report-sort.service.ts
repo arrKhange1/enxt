f3d1455c9e40e19f777d85f5eebe0fd236b122a4
arrKhange1 <therock65frostik@gmail.com>
2025-02-01 16:14:18 +0500

    refact: add comments to ReportSortService

--- src/cargo-report/service/report-sort.service.ts
+++ src/cargo-report/service/report-sort.service.ts
@@ -6,6 +6,14 @@ import { ReportSortUrlParams } from '../model/report.model';
 export class ReportSortService {
   private reportUrlDataService = inject(ReportUrlParamsDataService);
 
+  /**
+   * Метод с логикой изменения URL параметров сортировки
+   * Если порядок сортировки пустой, то сбрасываем сортировку (обнуляем порядок и поле сортировки)
+   * Если порядок не пустой, то применяем новый порядок сортировки для переданного поля
+   *
+   * @param sortOrder - Порядок сортировки
+   * @param sortName - Название поля, по которому сортируем
+   */
   public sortReport({ sortOrder, sortName }: ReportSortUrlParams): void {
     if (!sortOrder) {
       this.reportUrlDataService.patchUrlParams({
